f2fa4910919942d07d95da77f8aef17f050f06c5
Shubham Rathod <shubhamrathod1932@gmail.com>
2026-07-29 19:48:37 +0530

    Day 33 Completed

--- src/greeks.py
+++ src/greeks.py
@@ -32,3 +32,16 @@ def gamma(S, K, T, r, sigma):
     return norm.pdf(d1) / (
         S * sigma * np.sqrt(T)
     )
+
+def vega(S, K, T, r, sigma):
+
+    d1 = (
+        np.log(S / K)
+        + (r + sigma**2 / 2) * T
+    ) / (sigma * np.sqrt(T))
+
+    return (
+        S
+        * norm.pdf(d1)
+        * np.sqrt(T)
+    )
